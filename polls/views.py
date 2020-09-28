2de04d83ab26f3d1fe7d5378ecdcb86a701cdd85
Jacob <jacobhtnguyen@gmail.com>
2020-09-28 19:10:45 -0400

    updated index to use template and render response

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
-# Create your views here.
+from django.template import loader
 from polls.models import Question
 
 
+# Create your views here.
 def index(request):
+    # short version
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+    # full version
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
